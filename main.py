0484d8b1955a32aed9664e493640e17520ad5736
yellowsky24 <68938440+yellowsky24@users.noreply.github.com>
2021-10-04 21:21:27 +0900

    Update 2 on GitHub branch Only

--- main.py
+++ main.py
@@ -22,3 +22,6 @@ print("Hello World Update on GitHub branch")
 
 #adding new item in master branch
 print("Hello World Updates on Local Master Branch")
+
+#updates in main branch of GitHub
+print("Hello World Update 2 on GitHub branch")
